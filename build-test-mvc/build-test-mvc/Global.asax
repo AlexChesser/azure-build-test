96b93f21d9878965dc124d3a14d732256096123a
Alex Chesser <achesser@klick.com>
2017-03-06 12:45:56 -0500

    initial commit - template

---
+++ build-test-mvc/build-test-mvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="build_test_mvc.MvcApplication" Language="C#" %>
---
+++ build-test-mvc/build-test-mvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(build_test_mvc.Startup))]
+namespace build_test_mvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
